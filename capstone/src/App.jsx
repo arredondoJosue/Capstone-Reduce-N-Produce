31bb1b6ac86c1bf064a86800bfd4860fb6d6a95e
Josue Arredondo <josue.e.arredondo@gmail.com>
2022-04-22 11:13:40 -0600

    generated new API key firebase not installed yet

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithEmailAndPassword,
